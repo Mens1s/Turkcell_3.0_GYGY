727d59797f172aea031f095de50b437acc31b5ee
Ali <clkk.ali87@gmail.com>
2024-03-09 10:24:45 +0300

    CityServiceImpl.java updated

--- Homeworks/Homework-4/turkcellmarket/src/main/java/com/example/turkcellmarket/services/concretes/CityServiceImpl.java
+++ Homeworks/Homework-4/turkcellmarket/src/main/java/com/example/turkcellmarket/services/concretes/CityServiceImpl.java
@@ -3,9 +3,13 @@ package com.example.turkcellmarket.services.concretes;
 import com.example.turkcellmarket.entities.City;
 import com.example.turkcellmarket.repositories.abstracts.CityRepository;
 import com.example.turkcellmarket.services.abstracts.CityService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@AllArgsConstructor
+@Service
 public class CityServiceImpl implements CityService {
 
     private CityRepository cityRepository;
