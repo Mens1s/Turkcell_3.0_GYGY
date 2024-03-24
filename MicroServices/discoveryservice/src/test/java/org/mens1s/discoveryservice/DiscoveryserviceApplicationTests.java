4c3bf94c5e00ee5f285331db89fe53401d3e3b6d
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2024-03-25 00:44:51 +0300

    discovery service - eureka adde

---
+++ MicroServices/discoveryservice/src/test/java/org/mens1s/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mens1s.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
