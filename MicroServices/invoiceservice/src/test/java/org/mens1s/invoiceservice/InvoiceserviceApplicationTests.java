f21340c3e9417b182696a0d5f2ad232d3fea453a
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2024-03-24 23:05:50 +0300

    create service files

---
+++ MicroServices/invoiceservice/src/test/java/org/mens1s/invoiceservice/InvoiceserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mens1s.invoiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ MicroServices/productservice/src/test/java/org/mens1s/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mens1s.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
