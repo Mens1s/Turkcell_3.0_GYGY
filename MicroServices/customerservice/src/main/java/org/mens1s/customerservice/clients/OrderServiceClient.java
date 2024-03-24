4d3bcfb4f98d033e3cfdaf937515a6a2d79fc37f
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2024-03-25 00:55:52 +0300

    Docker files and docker compose added

--- MicroServices/customerservice/src/main/java/org/mens1s/customerservice/clients/OrderServiceClient.java
+++ MicroServices/customerservice/src/main/java/org/mens1s/customerservice/clients/OrderServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="orderservice", url="http://localhost:8081")
+@FeignClient(name="orderservice")
 public interface OrderServiceClient {
     @GetMapping("/api/orders")
     int getCustomerIdByOrderId(@RequestParam("orderId") String orderId);
